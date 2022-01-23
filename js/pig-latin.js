1bce97c2b21b1787ff9b639f6d93bbcac9fe99b7
Will W <wjwat@onionslice.org>
2022-01-22 19:57:55 -0600

    Copied over util functions from text-analyzer

--- js/pig-latin.js
+++ js/pig-latin.js
@@ -1,3 +1,18 @@
+function noWord(word) {
+  return (word.trim().length === 0)
+}
+
+function splitPassage(passage) {
+  return passage.split(' ');
+}
+
+function puncTrimWord(word) {
+  const c = '.,\/!?:;&'
+  const regex = new RegExp(`^[${c}]|[${c}]$`, 'g')
+
+  return word.replace(regex, '');
+}
+
 function pigLatin(passage) {
   let pigified = '';
   return pigified;
